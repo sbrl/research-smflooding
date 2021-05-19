23cbb225140ba9a5ad65dab0879c6a8410309a55
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-05-19 20:09:20 +0100

    Specify output signature

--- src/lib/data/TweetsData.py
+++ src/lib/data/TweetsData.py
@@ -7,13 +7,17 @@ from ..io.settings import settings_get
 from ..glove.glove import GloVe
 from .CategoryCalculator import CategoryCalculator
 
+# HACK
+glove = None
+
 
 class TweetsData(tf.data.Dataset):
     """Converts a jsonl tweets file into tensors."""
     
     def _generator():
+        global glove
+        
         settings = settings_get()
-        glove = GloVe(settings.data.paths.glove)
         cats = CategoryCalculator()
         reader = io.open(settings.data.paths.input, "r")
         
@@ -40,9 +44,20 @@ class TweetsData(tf.data.Dataset):
     
     def __new__(this_class):
         """Returns a new tensorflow dataset object."""
+        global glove
         settings = settings_get()
+        
+        # Globalise the instance
+        if glove is None:
+            glove = GloVe(settings.data.paths.glove)
+        
         
         return tf.data.Dataset.from_generator(
             this_class._generator,
-            args = ( )
+            output_signature=(
+                tf.TensorSpec(shape=(
+                    settings.data.sequence_length,
+                    glove.word_vector_length()
+                ), dtype="float32")
+            )
         ).batch(settings.train.batch_size).prefetch(tf.data.AUTOTUNE)
--- src/lib/glove/glove.py
+++ src/lib/glove/glove.py
@@ -24,6 +24,7 @@ class GloVe:
 		
 		self.data = {}
 		
+		self.word_length = None
 		self.filepath = filepath
 		self.load()
 	
@@ -40,6 +41,10 @@ class GloVe:
 				lambda el: float(el),
 				parts[1].split(" ")
 			))
+			if self.word_length is None:
+				self.word_length = len(self.data[parts[0]])
+			
+			# Update the CLI
 			if i % 10000 == 0:
 				sys.stderr.write(f"\rLoading GloVe from '{self.filepath}': {i}...")
 		
@@ -74,6 +79,15 @@ class GloVe:
 	def _normalise(self, str):
 		"""Normalises input text to be suitable to GloVe lookup."""
 		return normalise_text(str)
+	
+	
+	###########################################################################
+	
+	
+	def word_vector_length(self):
+		"""Returns the length of a single word vector."""
+		return self.word_length
+	
 	
 	def tweetvision(self, str):
 		"""
