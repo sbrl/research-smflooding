b04003945e7041c440857c3c880d7fe861a3f022
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-10-18 17:54:24 +0100

    LayerCCTConvEmbedding: override get_config
    ...not that you can tell it's an override, because Python is lovely like 
    that :-/

--- src/lib/ai/LayerCCTConvEmbedding.py
+++ src/lib/ai/LayerCCTConvEmbedding.py
@@ -6,23 +6,43 @@ class LayerCCTConvEmbedding(tf.keras.layers.Layer):
 		"""Initialises a new LayerCCTConvEmbedding layer instance."""
 		super(LayerCCTConvEmbedding, self).__init__(**kwargs)
 		
+		self.filters = filters
+		self.kernel = kernel
+		self.strides = strides
+		self.pool_kernel = pool_kernel
+		self.pool_stride = pool_stride
+		
 		self.submodel = tf.keras.Sequential()
 		
 		self.submodel.add(tf.keras.layers.Conv2D(
-			filters=filters, 
-			kernel_size=kernel,
-			strides=strides,
+			filters=self.filters, 
+			kernel_size=self.kernel,
+			strides=self.strides,
 			use_bias=False
 		))
 		self.submodel.add(tf.keras.layers.ReLU())
 		# The Keras tutorial adds ZeroPadding2D here, but the original doesn't? I'm confused
 		# Ref https://github.com/SHI-Labs/Compact-Transformers/blob/main/src/utils/tokenizer.py
 		self.submodel.add(tf.keras.layers.MaxPool2D(
-			pool_size=pool_kernel,
-			strides=pool_stride,
+			pool_size=self.pool_kernel,
+			strides=self.pool_stride,
 			padding="same"
 		))
-		
+	
+	
+	
+	def get_config(self):
+		"""
+		Returns this layer's configuration so that it can be serialised by Tensorflow.
+		Without this, Tensorflow will crash on saving a checkpoint O.o
+		"""
+		return {
+			"filters": self.filters,
+			"kernel": self.kernel,
+			"strides": self.strides,
+			"pool_kernel": self.pool_kernel,
+			"pool_stride": self.pool_stride
+		}
 	
 	def call(self, images):
 		return self.submodel(images)
