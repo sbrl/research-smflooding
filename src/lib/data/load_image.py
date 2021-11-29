8fced63be3789a1b0616dcbb4e346d3eaaff36ec
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-11-29 17:01:08 +0000

    load_image: be more robust

--- src/lib/data/load_image.py
+++ src/lib/data/load_image.py
@@ -1,13 +1,22 @@
 import tensorflow as tf
 
+from loguru import logger
+
+# Ref https://stackoverflow.com/a/23575424/1460422
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def load_image(filepath, image_size, model_type="resnet"):
-    
-    image = tf.keras.utils.load_img(
+    image = None
+    try:
+        image = tf.keras.utils.load_img(
         filepath,
         target_size=(image_size, image_size),
         color_mode="rgb"
-    )
+        )
+    except Exception as e:
+        logger.warn(f"Ignoring error: {e}")
+        return None
     
     
     if image_size < 32 and model_type == "resnet":
