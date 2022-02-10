39bbbc0c695135fe6cc500a1080e3122f4456b5e
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-02-10 18:28:54 +0000

    Lay out the basic structure for the CLIP-based classifier model
    text + images = better classification performance?
    
    hmmmm, we might want to snaffle the image alt text somehow if we're 
    using CLIP, I'm not sure.

---
+++ src_clip/CLIPClassifier.py
@@ -0,0 +1,1 @@
+# TODO: Implement class to manage the training (and prediction), using model.py to create the model in question.
---
+++ src_clip/dataset.py
@@ -0,0 +1,1 @@
+# TODO: Implement dataset loader here. We probably aso want to do the embeddings here too.
