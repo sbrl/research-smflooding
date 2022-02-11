a9840a42d21f3433cd5bc442fef3d38044b05e2a
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-02-11 18:49:44 +0000

    CLIP: implement core model & training/validation driver
    ....but there's still a loong way to go. Good gracious, PyTorch really 
    doesn't do anything for you, does it? I almost prefer Tensorflow, but 
    Tensorflow has terrible documentation.
    
    I can't win, can I?

--- src_clip/CLIPClassifier.py
+++ src_clip/CLIPClassifier.py
@@ -1,1 +1,67 @@
-# TODO: Implement class to manage the training (and prediction), using model.py to create the model in question.
+import torch
+
+import clip
+
+import model_clip from model
+
+class CLIPClassifier(object):
+    def __init__(self, dir_output, epochs=50, batch_size=64, **kwargs):
+        super(CLIPClassifier, self).__init__(**kwargs)
+        
+        self.dir_output = dir_output
+        self.epochs = epochs
+        self.batch_size = batch_size
+        
+        self.model = model_clip(**kwargs)
+        self.loss = torch.nn.CrossEntropyLoss()
+        self.optimiser = torch.optim.AdamW(self.model.parameters())
+    
+    def train(dataloader_train, dataloader_validate):
+        for epoch_i in range(self.epochs):
+            print(f"*** Epoch {epoch_i} ***")
+            loss, acc = self.__train(self, dataloader_train)
+            val_loss, val_acc = self.__validate(dataloader_validate)
+            
+            # TODO: Log the metrics in our standard format
+        
+        
+    
+    def __train(self, dataloader):
+        loss_total = 0
+        correct = 0
+        count_batches = 0
+        
+        for i, ((images, text), labels) in enumerate(dataloader):
+            predictions = self.model(images, text)
+            loss = self.loss(predictions, labels)
+            
+            self.optimiser.zero_grad()
+            self.loss.backward()
+            self.optimiser.step()
+            
+            loss_total += self.loss(predictions, labels).item()
+            correct += (predictions.argmax(1) == labels).type(torch.float).sum().item()
+            count_batches += 1
+            
+        return (loss_total / count_batches), (correct / (count_batches * self.batch_size))
+    
+    def __validate(self, dataloader):
+        """
+        The validation driver loop for a single epoch.
+        
+        returns: (number, number)   The results of the validation in the form (loss, accuracy). The accuracy is a number between 0 and 1.
+        """
+        loss_total = 0
+        correct = 0
+        count_batches = 0
+        
+        with torch.no_grad():
+            for i, ((images, text), labels) in enumerate(dataloader):
+                predictions = self.model(images, text)
+                loss_total += self.loss(predictions, labels).item()
+                correct += (predictions.argmax(1) == labels).type(torch.float).sum().item()
+                
+                count_batches += 1
+                
+        
+        return (loss_total / count_batches), (correct / (count_batches * self.batch_size))
--- src_clip/model.py
+++ src_clip/model.py
@@ -1,1 +1,32 @@
-# TODO: Implement the model in here.
+import torch
+
+import clip
+
+from PIL import Image
+
+class CLIP(torch.nn.Module):
+    def _init__(self, units=512, classes=2, device="cpu", **kwargs):
+        super(CLIP, self).__init__()
+        self.clip = clip.load("ViT-B/32", device=device)
+        self.clip_units_out = 512
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(512, units),
+            torch.nn.ReLU(),
+            torch.nn.Linear(units, units),
+            torch.nn.ReLU(),
+            torch.nn.Linear(units, classes),
+            torch.nn.Softmax()
+        ).to(device)
+    
+    def forward(self, text, images):
+        with torch.no_grad():
+            text_encoded = self.clip.encode_text(text)
+            images_encoded = self.clip.encode_image(images)
+        
+        # text_encoded and image_encoded should look like [ 1, 512 ]
+        result = torch.stack([text_encoded, image_encoded], 1)
+        
+        result = torch.adaptive_avg_pool1d(result, 1)
+            .squeeze(-1)
+        
+        return self.classifier(result)
