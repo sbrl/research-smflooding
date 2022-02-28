6fcd7504385094a0eb85ff251e4e821cc426c399
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-02-28 21:26:20 +0000

    hackathon: ensure code is fully detacted from parent codebase

--- hackathon-202203/glove/glove.py
+++ hackathon-202203/glove/glove.py
@@ -1,12 +1,36 @@
 import time
 import io
 import sys
-from loguru import logger
 import numpy
 import tensorflow as tf
-from ..polyfills.string import removeprefix, removesuffix
 
 from .normalise_text import normalise as normalise_text
+
+# / removed from being the last param, ref https://stackoverflow.com/a/56514307
+def removeprefix(self: str, prefix: str) -> str:
+    """
+    Removes a prefix from a string.
+    Polyfills string.removeprefix(), which is introduced in Python 3.9+.
+    Ref https://www.python.org/dev/peps/pep-0616/#specification
+    """
+    if self.startswith(prefix):
+        return self[len(prefix):]
+    else:
+        return self[:]
+
+
+# / removed from being the last param, ref https://stackoverflow.com/a/56514307
+def removesuffix(self: str, suffix: str) -> str:
+    """
+    Removes a suffix from a string.
+    Polyfills string.removesuffix(), which is introduced in Python 3.9+.
+    Ref https://www.python.org/dev/peps/pep-0616/#specification
+    """
+    # suffix='' should not call self[:-0].
+    if suffix and self.endswith(suffix):
+        return self[:-len(suffix)]
+    else:
+        return self[:]
 
 
 class GloVe:
@@ -115,7 +139,7 @@ class GloVe:
 		for i, token in enumerate(self._tokenise(str)):
 			embedding = self.lookup(token)
 			if embedding is None:
-				logger.debug(f"[DEBUG] {token} was none")
+				# logger.debug(f"[DEBUG] {token} was none")
 				continue
 			
 			result.append(embedding)
