fb5531b0fea5bae67d4bd6dd78318160a72499b3
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-04-01 14:50:41 +0100

    CLIP: Remove gc.collect()

--- src/lib/clip/CLIPImagePolyfiller.py
+++ src/lib/clip/CLIPImagePolyfiller.py
@@ -6,7 +6,6 @@ import datetime
 import sys
 import os
 import subprocess
-import gc
 
 from loguru import logger
 import torch
@@ -71,7 +70,6 @@ class CLIPImagePolyfiller(object):
 		# IMPORTANT: use with torch.no_grad(): before calling this function!!!
 		image_features = self.clip_model.encode_image(image_batch.to(self.device))
 		image_features /= image_features.norm(dim=-1, keepdim=True)
-		gc.collect()
 		return image_features
 	
 	
