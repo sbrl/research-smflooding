537afa55a2b043502c3653ec23a58a92f393dc27
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-05-24 19:50:27 +0100

    add glove-plot JS script that uses nodeplotlib

---
+++ scripts/glove-plot.mjs
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+"use strict";
+
+import fs from 'fs';
+
+import { plot } from 'nodeplotlib';
+import nexline from 'nexline';
+import cossim from 'cos-similarity';
+
+const words = [ "flood", "flooded", "cat", "tree" ];
+
+const glove_location = "/mnt/research-data/main/glove/glove.twitter.27B.25d.txt";
+
+let glove = {};
+
+const reader = nexline({ input: fs.createReadStream(glove_location) });
+let i = -1;
+while (true) {
+	i++;
+	const line = await reader.next();
+	if(line === null) break;
+	if(line.trim() === "") continue;
+	
+	const parts = line.split(" ");
+	glove[parts[0]] = parts.slice(1).map(value => parseFloat(value));
+	
+	if(i % 10000 === 0) process.stderr.write(`GloVe: Reading entry ${i}\r`);
+}
+
+const vectors = words.map(word => glove[word.toLowerCase()]);
+
+const similarities = vectors.map(vector => {
+	return vectors.map(other => cossim(vector, other));
+});
+
+console.log(`similarities`, similarities);
+
+plot([
+	// {
+	// 	y: words,
+	// 	z: vectors,
+	// 	type: "heatmap",
+	// 	name: `GloVe d25 word vectors`
+	// },
+	{
+		x: words,
+		y: words,
+		z: similarities,
+		type: "heatmap",
+		title: `GloVe d25 word cosine similarities`,
+		colorbar: {
+			tickmode: "linear",
+			tick0: 0,
+			dtick: 0.1
+		}
+	}
+]);
