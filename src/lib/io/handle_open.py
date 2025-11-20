2aaf3239cb75b1a32d6b548cc528e48f59c1853c
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2025-11-20 18:48:06 +0000

    handle_open: add optional zstd support for Python 3.14+

--- src/lib/io/handle_open.py
+++ src/lib/io/handle_open.py
@@ -2,11 +2,18 @@ import io
 import gzip
 
 
+try:
+	import compression.zstd as zstd
+except ImportError:
+	print("handle_open: Not Python 3.14 or above; zstd decompression is not supported --@sbrl")
+
 def handle_open(filepath, mode, force_textwrite_gzip=True):
 	if mode == "w" and mode.endswith(".gz") and force_textwrite_gzip:
 		mode = "wt"
 	
 	if filepath.endswith(".gz"):
 		return gzip.open(filepath, mode)
+	elif filepath.endswith(".zst") or filepath.endswith(".zstd") and zstd:
+		return zstd.open(filepath, mode)
 	else:
 		return io.open(filepath, mode)
